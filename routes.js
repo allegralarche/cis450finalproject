3ba1c8433e57638580fe9c40b7c990004e3a9aaf
ALIZA STONE <amstone326@gmail.com>
2015-04-06 13:32:35 -0400

    added grabbing startingAddress value, and starter code for parsing json dictionary

--- routes.js
+++ routes.js
@@ -19,28 +19,51 @@ exports.enterList = function(req, res) {
 //This route should be called (as a POST request) when the todo list is submitted
 exports.processList = function(req, res) {
 	var todolist = req.body.list;
-	getBusinessesForList(todolist, function(businessesStruct) {
-		getBestBusinessesAlgorithm(businessesStruct, function(bestBusinesses, unsatisfiedItems) {
+	var startAddress = req.body.startingAddress;
+	getBusinessesForList(todolist, startAddress, function(businessesStruct) {
+		getBestBusinessesAlgorithm(businessesStruct, startAddress, function(bestBusinesses, unsatisfiedItems) {
 			res.send({businesses:bestBusinesses, unsatisfiedItems:unsatisfiedItems});
 		});
 	});
 };
 
 
-//NINA'S CODE
-var getBusinessesForList = function(todoList, callback) {
+//NINA'S CODE -- arguments passed in are the user's todo list items, and the user's starting address
+var getBusinessesForList = function(todoList, startAddress, callback) {
 	//A dictionary that maps todo_item --> a set of json structures representing the businesses at which
 	//you could complete that todo_item
-	var itemsToBusinesses = {};
+	var itemsToBusinesses = {}; 
 	
 	callback(itemsToBusinesses);
 };
 
+/*
+ * Expected format of itemsToBusinesses:
+ * var item_dict = '{"buy_shirt":' +  
+	'[ {"name":"Target", "bid":"hfsd7383fj9", "address":"4000 Pine Street", "rating":"3.5", "lat":"0.0", "long":"0.0"},' +
+     '{"name":"Walmart", "bid": "hfg3653fj9", "address": "200 South Columbus", "rating":"4.0", "lat":"0.0", "long":"0.0"} ],' + 
+	'"get haircut":' +
+	'[ {"name":"Kidz Cutz", "bid":"hfsd7383fj9", "address":"4000 Pine Street", "rating":"3.5", "lat":"0.0", "long":"0.0"},' +
+     '{"name":"Bob Barber", "bid": "hfg3653fj9", "address": "200 South Columbus", "rating":"4.0", "lat":"0.0", "long":"0.0"} ] }';
+ */
 
 //ALIZA AND JARED'S CODE
-var getBestBusinessesAlgorithm = function(itemsToBusinesses, callback) {
+var getBestBusinessesAlgorithm = function(itemsToBusinesses, startAddress, callback) {
 	bestBusinesses = [];
 	unsatisfiedToDos = [];
+	
+	
+	
+	var obj = JSON.parse(itemsToBusinesses);
+	
+	for (var item in obj) {
+		console.log(item);
+		var business_list = obj[item];
+		for (var  i = 0; i < business_list.length; i++) {
+			console.log(business_list[i].name);
+		}
+	}
+	
 	callback(bestBusinesses, unsatisfiedToDos);
 };
 

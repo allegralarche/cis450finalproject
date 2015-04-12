5b4c1b825c8b9a32af1165ddd3bd20de97e6eb87
Allegra Larche <allegralarche@gmail.com>
2015-04-12 16:36:19 -0400

    fixed styling hopefully

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ app.set('views', path.join( __dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true }));
 app.use(bodyParser.json());
+app.use("/styles",express.static(__dirname + "/styles"));
 
 routeFile.init(function() {
 	app.get('/', routeFile.enterList);
